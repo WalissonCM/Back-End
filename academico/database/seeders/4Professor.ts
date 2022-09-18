c43616a6ba915da161f4f6f91486212fc79781d2
WalissonCM <walissoncavalcante@outlook.com>
2022-09-18 16:36:29 -0300

    teste

--- academico/database/seeders/4Professor.ts
+++ academico/database/seeders/4Professor.ts
@@ -4,10 +4,16 @@ import Professor from 'App/Models/Professor'
 export default class extends BaseSeeder {
   public async run () {
     await Professor.createMany([
-      {nome: "Danilo", cpf: 19863257345, matricula: "247524", salario: "R$8000", email: "danili@email.com", telefone: "61-89245658",
-       cep: 8965441, logadouro: "qnp09", complemento: "38A", numero: "61-9856472", bairro: "mineiro"},
-      {nome: "Fabio", cpf: 19863512345, matricula: "57546", salario: "R$8000", email: "fabio@email.com", telefone: "61-98567423",
-       cep: 6587412, logadouro: "qnp65", complemento: "45C", numero: "61-9563145", bairro: "salvado"},
+      {nome: "Alex", cpf: 78945612300, matricula: "2087432165", salario: "R$10.000", email: "Alex@gmail.com", telefone: "61-40028922",
+       cep: 72250300, logadouro: "qnq05", complemento: "Cas", numero: "30", bairro: "Ceilandia"},
+      {nome: "Carlos", cpf: 78945612301, matricula: "187432165", salario: "R$10.000", email: "Carlos@gmail.com", telefone: "61-61-40028925",
+       cep: 72430260, logadouro: "Quadra 3 Área D", complemento: "Apt", numero: "12", bairro: "Gama"},
+      {nome: "Matheus", cpf: 78945612302, matricula: "1587432165", salario: "R$10.000", email: "Matheus@gmail.com", telefone: "61-40028929",
+       cep: 70330515, logadouro: "Quadra CLS 102 Bloco A", complemento: "Apt", numero: "44", bairro: "Asa Sul"},
+      {nome: "Barbara", cpf: 78945612303, matricula: "1287432165", salario: "R$10.000", email: "Barbara@gmail.com", telefone: "61-40028927",
+       cep: 71070700, logadouro: "Área Especial 10", complemento: "Cas", numero: "6", bairro: "Tagutainga"},
+      {nome: "Camila", cpf: 78945612305, matricula: "9287432165", salario: "R$10.000", email: "Camila@gmail.com", telefone: "61-40028924",
+       cep: 70297-400, logadouro: "Quadra 101", complemento: "Cond", numero: "25", bairro: "Aguas Claras"},
     ])
   }
 }
--- academico/database/seeders/5Aluno.ts
+++ academico/database/seeders/5Aluno.ts
@@ -4,10 +4,26 @@ import Aluno from 'App/Models/Aluno'
 export default class extends BaseSeeder {
   public async run () {
     await Aluno.createMany([
-      {nome: "Renato", cpf: 1266765312, matricula: "21114286787", email: "renato@email.com", telefone: "61-98275869",
-       cep: 7521542, logadouro: "qnp15", complemento: "35B", numero: "61-98275865", bairro: "passarinho"},
-      {nome: "Orion", cpf: 10266765312, matricula: "5447874547", email: "orion@emaul.com", telefone: "61-985474547",
-       cep: 7285896, logadouro: "qnp65", complemento: "39A", numero: "61-98754514", bairro: "belo"},
+      {nome: "Alisson", cpf: 35149863200, matricula: "922100289", email: "Alisson@outlook.com", telefone: "61-33725637",
+       cep: 72210-502, logadouro: "CNM 2", complemento: "Cas", numero: "65B", bairro: "Ceilandia"},
+      {nome: "Davi", cpf: 35149863201, matricula: "925100289", email: "Davi@outlook.com", telefone: "61-33745638",
+       cep: 72210-542, logadouro: "CNR 1", complemento: "Cas", numero: "2A", bairro: "Ceilandia"},
+      {nome: "Luis", cpf: 35149863202, matricula: "926100289", email: "Luis@outlook.com", telefone: "61-32745637",
+       cep: 72215-523, logadouro: "EQNM 6", complemento: "Apt", numero: "10C", bairro: "Ceilandia"},
+      {nome: "Ronaldi", cpf: 35149863203, matricula: "928100289", email: "Ronaldi@outlook.com", telefone: "61-31745637",
+       cep: 72255-545, logadouro: "EQNO 11/14", complemento: "Cas", numero: "15b", bairro: "Ceilandia"},
+      {nome: "Lucas", cpf: 35149863204, matricula: "927100289", email: "Lucas@outlook.com", telefone: "61-33745537",
+       cep: 72252-500, logadouro: "EQNP 9", complemento: "Apt", numero: "40A", bairro: "Ceilandia"},
+      {nome: "Maria", cpf: 35149863205, matricula: "921100289", email: "Maria@outlook.com", telefone: "61-33745647",
+       cep: 72265-501, logadouro: "QCS 19", complemento: "Apt", numero: "55B", bairro: "Ceilandia"},
+      {nome: "Gabriel", cpf: 35149863206, matricula: "924600289", email: "Gabriel@outlook.com", telefone: "61-33795637",
+       cep: 72220-080, logadouro: "QNM 8", complemento: "Cas", numero: "33C", bairro: "Ceilandia"},
+      {nome: "Naiara", cpf: 35149863207, matricula: "980100289", email: "Naiara@outlook.com", telefone: "61-33745617",
+       cep: 72255-304, logadouro: "QNO 13", complemento: "Cond", numero: "29A", bairro: "Ceilandia"},
+      {nome: "Vitor", cpf: 35149863208, matricula: "970100289", email: "Vitor@outlook.com", telefone: "61-33745690",
+       cep: 72231-101, logadouro: "QNP 10", complemento: "Apt", numero: "11A", bairro: "Ceilandia"},
+      {nome: "Monica", cpf: 35149863209, matricula: "950100289", email: "Monica@outlook.com", telefone: "61-337456312",
+       cep: 72270-302, logadouro: "QNQ 3", complemento: "Cond", numero: "6B", bairro: "Ceilandia"},
     ])
   }
 }
